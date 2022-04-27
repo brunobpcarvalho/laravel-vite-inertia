188cc603871c5ab3b5afff80777849c46d927e2a
brunobpcarvalho <bruno.bpcarvalho@gmail.com>
2022-04-27 13:59:05 -0300

    Adiciona plugins de desenvolvimento.

--- resources/scripts/shims-vue.d.ts
+++ resources/scripts/shims-vue.d.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // This is required for Visual Studio Code to recognize
 // imported .vue files
 declare module '*.vue' {
-	import { DefineComponent } from 'vue'
-	const component: DefineComponent<{}, {}, any>
-	export default component
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
